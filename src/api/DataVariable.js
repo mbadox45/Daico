3240d0080c42e02968d8ef72920be649fa4fce3f
unknown <mbadox45@gmail.com>
2024-08-30 15:29:38 +0700

    ganti url

--- src/api/DataVariable.js
+++ src/api/DataVariable.js
@@ -3,8 +3,8 @@ const date_now = moment().format('DD-MM-YYYY')
 const date_ago =  moment().subtract(10, 'days').format('DD-MM-YYYY');
 
 // Public Access
-// export const URL_WEB = 'http://36.92.181.10:38090/';
-export const URL_WEB = 'http://localhost:8481/';
+export const URL_WEB = 'http://36.92.181.10:38090/';
+// export const URL_WEB = 'http://localhost:8481/';
 // export const URL_API = 'https://daicoapi.salvaco.id/api/';
 // export const URL_API = 'http://192.168.4.88:8081/api/';
 export const URL_API = 'http://36.92.181.10:30008/api/';
